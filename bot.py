6625eb59e95eb577f4422963f4aab56de1e86eaf
Ivan <ivanleomk@gmail.com>
2019-10-11 16:30:00 +0800

    Added the following features
    1) Long Polling with 30s pause
    2) Instructions
    3) Ability to use tmrw and today shorthand for inputing dates
    4) Date-Time formatting
    
    To-Do
    
    1. Add support for deletion operations
    2. Sort SQL list by date ( so most urgent ones listed first )
    3. Nicer formatting for messages!
    
    N1

--- bot.py
+++ bot.py
@@ -2,59 +2,92 @@ import telepot
 import csv
 from pprint import pprint
 import database as dbb
-from datetime import date
+import datetime as datetime
 import os.path
+import time
 
 #Definining fun_stuff
 token = "964928453:AAFiGYtt7oUGzF4tSl59wMW7yE0VdhcW_lg"
 TelegramBot = telepot.Bot(token)
 
-#We grab data from the telegram bot and find all of our latest tasks that we have sent in
-updates = TelegramBot.getUpdates(3992767)
-#We then format the updates we recieve. They will be passed in using the
-#<Name> , <Deadline>
-for i in updates:
+
+
+#Defining FUnctions
+
+def format_date(date_Str):
+    date_obj= date_Str.strip().split("-")
+    if len(date_obj) == 1:
+        if date_obj[0] == "today":
+            return date.today()
+        elif date_obj[0] == "tmrw":
+            return datetime.date.today() + datetime.timedelta(days=1)
+    #FIrst case: We have specific wrods
+    elif len(date_obj) == 2:
+    #Second case : We have DD-MM
+        now = datetime.datetime.now()
+        date = "-".join(date_obj) + str(now.year) + "2019 23:59"
+        date_obj = datetime.datetime.strptime(date_time_str, '%d-%m-%Y %H:%M')
+    #Third-Case we have DD-MM-YY
+    elif len(date_obj) == 3:
+        date = "-".join(date_obj).strip()
+        date_obj = datetime.datetime.strptime(date, '%d-%m-%Y')
+    return date_obj
+
+def add_task(i):
     user_id = int(i['message']['from']['id'])
     name = i['message']["from"]["first_name"]
     task = i['message']['text'].split(',')
-    print(user_id)
-    print(dbb.check_user(user_id))
+    chat_id = i['message']['chat']["id"]
+    update_id = i['update_id']
+    print("The Update id is "+str(update_id))
     if(dbb.check_user(user_id)):
         pass
     else:
-        dbb.create_user(user_id,name)
-    dbb.create_task(user_id, task[1],task[2])
-    dbb.show_tasks()
+        dbb.create_user(user_id,name,update_id)
+    print(task[2])
+    date = format_date(task[2])
+    print(date)
+    dbb.create_task(user_id, task[1],date,update_id)
+    TelegramBot.sendMessage(chat_id, "Your Task <<{}>> has been added!".format(task[1]))
 
+#TO_DO : ADD A REMOVAL FUNCTION
+def remove_task(i):
+    pass
 
+def show_tasks(chat_id):
+    tasks = dbb.get_tasks()
+    TelegramBot.sendMessage(chat_id,
+    "Your *Current Tasks* are : \n "+tasks, parse_mode= "Markdown")
 
-# for i in updates:
-#     temp = []
-#     temp.append(str(i["update_id"]))
-#     #We then proccess the string according to <Type> <Name> <Deadline> <Description> format
-#     full_chunk = i["message"]["text"]
-#     temp.extend(format_text(full_chunk))
-#     tasks.append(temp)
-# #We then store the latest task_ID in the data_file
-# current_id+=len(tasks)
-#
-# with open("data.txt","w") as f:
-#     new_id = str(current_id)
-#     f.write(new_id)
-#
-#
-# #We check if the file we want to write to exists. If it does, we append to the existing file, otherwise, we create the file and write the tasks to a new file.
-# name = date.today().strftime("%d-%m") + ".csv"
-# check_path = "/Users/ivan/Documents/Personal/Raw_input_Telegram/csv/" + name
-# print(name)
-# method = "w"
-# if os.path.exists(check_path):
-#     method = "a"
-# else:
-#     method = "w"
-#
-# with open(check_path,method) as csv_file:
-#     writer = csv.writer(csv_file)
-#     writer.writerows(tasks)
-#
-# write_to_org();
+def main():
+    #We grab data from the telegram bot and find all of our latest tasks that we have sent in
+    update_id = dbb.get_user_latest_id()
+    user = 0
+    print("We have initialised the update_id to be {}".format(update_id))
+    updates = TelegramBot.getUpdates(update_id)
+    #We then format the updates we recieve. They will be passed in using the
+    #<Name> , <Deadline>
+    for i in updates:
+        pprint(i)
+        command = i['message']['text']
+        chat_id = i['message']['chat']["id"]
+        user = int(i['message']['from']['id'])
+        if command == '/get_tasks':
+            show_tasks(chat_id)
+        elif command == '/start':
+            TelegramBot.sendMessage(chat_id,
+            "Welcome to the *HelpLa Bot*! I was created to help deal with the huge chunk of to-dos that we need to work with in our day to day life \n Here is how to use the bot \n 1. To Add a To-Do Item, simply write it in as such *Task*,*Name Of Task*,*Deadline* \n 2. To get a list of current items, use the command \\get_tasks ",parse_mode='Markdown')
+        else:
+            add_task(i)
+        update_id = i['update_id']
+
+        user_id = user_id = int(i['message']['from']['id'])
+        dbb.update_user_update_id(update_id,user_id)
+        print("The new user ID is {}".format(dbb.get_user_latest_id()))
+
+    dbb.update_user_update_id(dbb.get_user_latest_id()+1, user)
+
+if __name__ == "__main__":
+    while True:
+        main()
+        time.sleep(30)
--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 import mysql.connector as mysql
+import datetime
+
 
 db = mysql.connect(
     host = "localhost",
@@ -14,9 +16,29 @@ def reset_tables():
 #Run this if you are running python bot for the first time
 def create_tables():
     cursor = db.cursor(buffered=True)
-    cursor.execute("CREATE TABLE tasks (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, task_name VARCHAR(255), Deadline VARCHAR(255), user INT(11))")
-    cursor.execute("CREATE TABLE users (id INT(11) PRIMARY KEY, Name VARCHAR(255))")
+    cursor.execute("CREATE TABLE tasks (update_id INT(11) PRIMARY KEY, task_name VARCHAR(255), Deadline DATETIME, user INT(11))")
+    cursor.execute("CREATE TABLE users (id INT(11) PRIMARY KEY, Name VARCHAR(255), last_update_id INT(11))")
     cursor.close()
+
+def get_user_latest_id():
+    cursor = db.cursor(buffered = True)
+    cursor.execute("SELECT last_update_id FROM users")
+    user = cursor.fetchall()
+    return user[0][0]
+
+def update_user_update_id(update_Id,user_Id):
+    cursor = db.cursor(buffered = True)
+    currID = get_user_latest_id()
+    if update_Id > currID:
+        query = "UPDATE users SET last_update_id = {} WHERE id = {}".format(str(update_Id),str(user_Id))
+        cursor.execute(query)
+        db.commit()
+        cursor.close()
+        print("User ID Updated")
+    else:
+        print("User ID not updated")
+
+update_user_update_id(3992779,155624230)
 
 def check_user(user_Id):
     cursor = db.cursor(buffered=True)
@@ -29,10 +51,10 @@ def check_user(user_Id):
     else:
         return False
 
-def create_user(user_id,user_name):
+def create_user(user_id,user_name,update_Id):
     cursor = db.cursor(buffered=True)
-    query = "INSERT INTO users (id, Name) VALUES (%s, %s)"
-    values = (user_id, user_name)
+    query = "INSERT INTO users (id, Name,last_update_id) VALUES (%s, %s,%s)"
+    values = (user_id, user_name,update_Id)
     cursor.execute(query,values)
     db.commit()
     cursor.close()
@@ -52,6 +74,7 @@ def show_users():
         print(user)
     cursor.close()
 
+
 def show_tasks():
     cursor = db.cursor(buffered=True)
     cursor.execute("SELECT * FROM tasks")
@@ -61,19 +84,34 @@ def show_tasks():
         print(task)
     cursor.close()
 
-def create_task(user_id,task_name, deadline):
-    cursor = db.cursor(buffered=True)
-    #cursor.execute("CREATE TABLE tasks (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, task_name VARCHAR(255), Deadline VARCHAR(255))")
-    ## defining the Query
-    query = "INSERT INTO tasks (user,task_name, Deadline) VALUES (%s,%s, %s)"
-    ## storing values in a variable
-    values = (str(user_id), task_name, deadline)
-    ## executing the query with values
-    cursor.execute(query, values)
-    ## to make final output we have to run the 'commit()' method of the database object
-    db.commit()
-    print(cursor.rowcount, "record inserted")
-    cursor.close()
+def check_task(up_id):
+    cursor = db.cursor(buffered= True)
+    query = "SELECT * FROM tasks WHERE update_id ="+str(up_id)
+    cursor.execute(query)
+    result = cursor.fetchall()
+    print(result)
+    if len(result)==1:
+        return True
+    else:
+        return False
+
+def create_task(user_id,task_name, deadline,up_id):
+    if check_task(up_id):
+        print("Tasks Exists")
+        pass
+    else:
+        cursor = db.cursor(buffered=True)
+        #Defining Query
+        query = "INSERT INTO tasks (update_id,user,task_name, Deadline) VALUES (%s,%s,%s, %s)"
+        ## storing values in a variable
+    
+        values = (str(up_id),str(user_id), task_name, deadline)
+        ## executing the query with values
+        cursor.execute(query, values)
+        ## to make final output we have to run the 'commit()' method of the database object
+        db.commit()
+        print(cursor.rowcount, "record inserted")
+        cursor.close()
 
 def show_tables():
     cursor = db.cursor(buffered=True)
@@ -85,6 +123,20 @@ def show_tables():
         print(table)
     cursor.close()
 
+def get_tasks():
+    cursor = db.cursor(buffered = True)
+    cursor.execute("SELECT task_name, Deadline FROM tasks")
+    tasks = cursor.fetchall()
+    sumTasks = "\n"
+    count = 1
+    for task in tasks:
+        name = task[0]
+        deadline = task[1].strftime('%m/%d/%Y')
+        sumTasks += "{}. {} ( Deadline: {} )\n".format(count,name, deadline)
+        count+=1
+    sumTasks+="\n"
+    return sumTasks
+
 def get_user_tasks(user_id):
     cursor = db.cursor(buffered=True)
     ## defining the Query
@@ -95,6 +147,3 @@ def get_user_tasks(user_id):
     tasks = cursor.fetchall()
     cursor.close()
     return tasks
-
-
-show_tasks()
